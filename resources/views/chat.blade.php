108805724922b64c2e4f4b374627f2fcb2d1d51e
ahmadelmalah <ahmedelmalah@hotmail.com>
2019-04-24 20:50:41 +0200

    Initial chat page

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <title>Hi chat</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row" id="app">
+            <h1>ahmed</h1>
+        </div>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html
