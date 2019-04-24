6bd64eeb219931feb57a199627e8851d0f032e29
ahmadelmalah <ahmedelmalah@hotmail.com>
2019-04-24 20:59:53 +0200

    setup chat component

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -8,7 +8,15 @@
 <body>
     <div class="container">
         <div class="row" id="app">
-            <h1>ahmed</h1>
+
+            <ul class="list-group col-4 offset-md-4">
+                <li class="list-group-item active">Chat Room</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+                <input type="text" class="form_control" placeholder="Type your message" />
+            </ul>
         </div>
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
